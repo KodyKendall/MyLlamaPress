f8802b232d4afafef15383b17fc83bae424bcc3f
Kody <kodyckendall@gmail.com>
2024-10-24 16:47:19 -0700

    feat: publish to wordpress, schema from migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_24_003742) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_24_231531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -104,6 +104,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_24_003742) do
     t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "wordpress_api_encoded_token"
     t.index ["organization_id"], name: "index_sites_on_organization_id"
     t.index ["slug"], name: "index_sites_on_slug", unique: true
   end
